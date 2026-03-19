9a84af24ee5aecb75e83e5714b00234f82b618fa
dandan812 <dandan812@users.noreply.github.com>
2026-03-19 08:40:37 +0800

    简化依赖，移除未使用的包

--- backend-code/src/app.js
+++ backend-code/src/app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
-const morgan = require('morgan');
 require('dotenv').config();
 
 const accessKeyMiddleware = require('./middlewares/accessKey');
@@ -11,9 +8,6 @@ const categoryRoutes = require('./routes/category');
 const app = express();
 
 app.use(cors());
-app.use(helmet());
-app.use(compression());
-app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
